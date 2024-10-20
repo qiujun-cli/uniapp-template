ea5d4f13ba4b49dd97844306ad9c4142ab2e4b9a
June <1601745371@qq.com>
2024-10-20 11:04:58 +0800

    chore: 🚀 优化unocss配置

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,18 @@
+/*
+ * @Author: June
+ * @Description:
+ * @Date: 2024-10-20 10:57:10
+ * @LastEditors: June
+ * @LastEditTime: 2024-10-20 11:03:49
+ * @FilePath: /uniapp-template/uno.config.ts
+ */
 import presetWeapp from 'unocss-preset-weapp'
+import { transformerDirectives } from 'unocss'
 import {
   extractorAttributify,
   transformerClass
 } from 'unocss-preset-weapp/transformer'
+import transformerCompileClass from '@unocss/transformer-compile-class' // 将一组类编译为一个类
 
 const { presetWeappAttributify, transformerAttributify } =
   extractorAttributify()
@@ -17,14 +27,16 @@ export default {
   shortcuts: [
     {
       'f-center': 'flex justify-center items-center',
-      'wh-full': 'w-full h-full'
+      'wh-full': 'w-full h-full',
+      'text-over-1': 'overflow-hidden text-ellipsis whitespace-nowrap'
     }
   ],
 
   transformers: [
+    transformerDirectives(),
+    transformerCompileClass(),
     // https://github.com/MellowCo/unocss-preset-weapp/tree/main/src/transformer/transformerAttributify
     transformerAttributify(),
-
     // https://github.com/MellowCo/unocss-preset-weapp/tree/main/src/transformer/transformerClass
     transformerClass()
   ]
