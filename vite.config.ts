7d6489cc635fa44de8eca2c4621314a5818fef34
June <1601745371@qq.com>
2025-01-28 14:14:32 +0800

    chore: 🚀 优化脚手架

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Description:
  * @Date: 2025-01-28 11:28:47
  * @LastEditors: June
- * @LastEditTime: 2025-01-28 13:58:19
+ * @LastEditTime: 2025-01-28 14:13:26
  * @FilePath: /uniapp-template/vite.config.ts
  */
 import { defineConfig, loadEnv } from 'vite'
@@ -12,6 +12,7 @@ import { resolve } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 import UniManifest from '@uni-helper/vite-plugin-uni-manifest'
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, resolve(process.cwd(), 'env'))
@@ -20,7 +21,15 @@ export default ({ mode }) => {
   return defineConfig({
     envDir: './env',
     plugins: [
-      UniManifest,
+      UniManifest(),
+      UniPages({
+        exclude: ['**/components/**/**.*'],
+        routeBlockLang: 'json5', // 虽然设了默认值，但是vue文件还是要加上 lang="json5", 这样才能很好地格式化
+        // homePage 通过 vue 文件的 route-block 的type="home"来设定
+        // pages 目录为 src/pages，分包目录不能配置在pages目录下
+        // subPackages: ['src/pages-sub'], // 是个数组，可以配置多个，但是不能为pages里面的目录
+        dts: 'src/types/uni-pages.d.ts'
+      }),
       uni(),
       Unocss(),
       AutoImport({
